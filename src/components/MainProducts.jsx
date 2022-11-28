4d0f1264a17cbf8de1068fcaac18616d71bac891
hwakk <x_xhwakk@naver.com>
2022-11-28 17:13:36 +0900

    feat: React query 개발

---
+++ src/components/MainProducts.jsx
@@ -0,0 +1,36 @@
+import { useQueryClient } from "@tanstack/react-query";
+import React, { useState } from "react";
+import Products from "./Products";
+
+function MainProducts() {
+  const [showLeftProducts, setShowLeftProducts] = useState(true);
+  const [showRightProducts, setShowRightProducts] = useState(true);
+
+  const client = useQueryClient();
+
+  return (
+    <main className="container">
+      <div>
+        {showLeftProducts && <Products />}
+        <button onClick={() => setShowLeftProducts((show) => !show)}>
+          Toggle
+        </button>
+      </div>
+      <div>
+        {showRightProducts && <Products />}
+        <button onClick={() => setShowRightProducts((show) => !show)}>
+          Toggle
+        </button>
+      </div>
+      <button
+        onClick={() => {
+          client.invalidateQueries(["products"]);
+        }}
+      >
+        정보가 업데이트 되었음
+      </button>
+    </main>
+  );
+}
+
+export default MainProducts;
